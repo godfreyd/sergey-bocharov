129ed0343c703e132c0e19eeb1d2448302c1059b
Sergey Bocharov <sergei-b84@bk.ru>
2022-11-06 18:51:50 +0300

    Fixes

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,37 +2,46 @@ import React, { FC } from 'react';
 import Link from 'next/link';
 import styles from './Footer.module.scss';
 
+const links = [
+    {
+        name: 'Twitter',
+        src: '/twitter.svg',
+        href: 'https://twitter.com/bocharov__s'
+    },
+    {
+        name: 'Linkedin',
+        src: '/linkedin.svg',
+        href: 'https://www.linkedin.com/in/bocharovsergey/'
+    },
+    {
+        name: 'GitHub',
+        src: '/github.svg',
+        href: 'https://github.com/godfreyd'
+    },
+]
+
+
 export const Footer: FC = () => (
     <footer className={styles.Footer}>
         <p>
             Follow me on{' '}
-            {/* <Link
-                href="https://codepen.io/ibemed"
-                rel="noopener noreferrer"
-                target="_blank"
-            >
-                <a>
-                    <img alt="CodePen" src="/CodePen.svg" />
-                    CodePen
-                </a>
-            </Link>{' '}
-            or{' '} */}
-            <Link
-                href="https://www.linkedin.com/in/bocharovsergey/"
-                rel="noopener noreferrer"
-            >
-                <a target="_blank">
-                    <img alt="LinkedIn" src="/Linkedin.svg" />
-                    Linkedin
-                </a>
-            </Link>{' '}
-            or{' '}
-            <Link href="https://github.com/godfreyd" rel="noopener noreferrer">
-                <a target="_blank">
-                    <img alt="GitHub" src="/GitHub.svg" />
-                    GitHub
-                </a>
-            </Link>{' '}
+                {
+                    links.map((item) => (
+                        <span  key={item.name}>
+                            <Link
+                                href={item.href}
+                                rel="noopener noreferrer"
+                                target="_blank"
+                            >
+                                <a>
+                                    <img alt="Twitter" src={item.src} />
+                                    {item.name}
+                                </a>
+                            </Link>
+                            {(item.name === 'GitHub') ? ' ' : ', '}
+                        </span>
+                    ))
+                }
             if you want, I suppose. Also you can{' '}
             <a href="mailto:sergei-b84@bk.ru">write me</a> .
         </p>
