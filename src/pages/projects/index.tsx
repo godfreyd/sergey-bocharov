37a336be581d643b284146f52e829a1bf9049146
Sergey Bocharov <sergey.bocharov@emergingtravel.com>
2024-08-23 07:42:28 +0100

    Update project page

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -6,62 +6,74 @@ import { Title } from '../../components/Title';
 import { Section } from '../../components/Section';
 
 function Projects() {
-    return (
-        <BaseLayout>
-            <Head>
-                <title>Sergey Bocharov - Projects</title>
-                <meta
-                    name="viewport"
-                    content="initial-scale=1.0, width=device-width"
-                />
-            </Head>
-            <Title tag="h1">Projects</Title>
-            <Section>
-                <ol>
-                    <li>
-                        <Link target="_blank" href="https://github.com/LookingSchools/ui">
-                            <code>@lookingschools/ui</code>
-                        </Link>
-                        <p>
-                            UI Kit build on React.{' '}
-                            <Link target="_blank" href="http://godfreyd.github.io/dist/?path=/story/amountpicker--playground">
-                                Demo
-                            </Link>
-                            .
-                        </p>
-                    </li>
-                    <li>
-                        <Link target="_blank" href="https://github.com/LookingSchools/ui">
-                            <code>ls-ui</code>
-                        </Link>
-                        <p>UI Kit build on React with CSS in JS.</p>
-                    </li>
-                    <li>
-                        <Link target="_blank" href="https://www.npmjs.com/package/html-csp-hash-generator">
-                            <code>html-csp-hash-generator</code>
-                        </Link>
-                        <p>
-                            Tool to generate hash for inline scripts and styles
-                            for CSP.
-                        </p>
-                    </li>
-                    <li>
-                        <Link target="_blank" href="https://github.com/bem/yandex-ui">
-                                <code>@yandex/ui</code>
-                        </Link>
-                        <p>Yandex UI Kit build on React and bem-react.</p>
-                    </li>
-                    <li>
-                        <Link target="_blank" href="https://github.com/godfreyd/react-redux-ssr">
-                            <code>react-redux-ssr</code>
-                        </Link>
-                        <p>SSR build on React.</p>
-                    </li>
-                </ol>
-            </Section>
-            <Button onClick={() => window.history.back()}>Back</Button>
-        </BaseLayout>
-    );
+  return (
+    <BaseLayout>
+      <Head>
+        <title>Sergey Bocharov - Projects</title>
+        <meta
+          name="viewport"
+          content="initial-scale=1.0, width=device-width"
+        />
+      </Head>
+      <Title tag="h1">Projects</Title>
+      <Section>
+        <ol>
+          <li>
+            <Link target="_blank" href="https://www.npmjs.com/package/colibri-ui">
+              <code>colibri-ui</code>
+            </Link>
+            <p>
+              React UI components library.{' '}
+              <Link target="_blank" href="https://godfreyd.github.io/colibri-ui/?path=/story/ui-badge--default">
+                Demo
+              </Link>
+              .
+            </p>
+          </li>
+          <li>
+            <Link target="_blank" href="https://github.com/LookingSchools/ui">
+              <code>@lookingschools/ui</code>
+            </Link>
+            <p>
+              UI Kit build on React.{' '}
+              <Link target="_blank" href="http://godfreyd.github.io/dist/?path=/story/amountpicker--playground">
+                Demo
+              </Link>
+              .
+            </p>
+          </li>
+          <li>
+            <Link target="_blank" href="https://github.com/LookingSchools/ui">
+              <code>ls-ui</code>
+            </Link>
+            <p>UI Kit build on React with CSS in JS.</p>
+          </li>
+          <li>
+            <Link target="_blank" href="https://www.npmjs.com/package/html-csp-hash-generator">
+              <code>html-csp-hash-generator</code>
+            </Link>
+            <p>
+              Tool to generate hash for inline scripts and styles
+              for CSP.
+            </p>
+          </li>
+          <li>
+            <Link target="_blank" href="https://github.com/bem/yandex-ui">
+              <code>@yandex/ui</code>
+            </Link>
+            <p>Yandex UI Kit build on React and bem-react.</p>
+          </li>
+          <li>
+            <Link target="_blank" href="https://github.com/godfreyd/react-redux-ssr">
+              <code>react-redux-ssr</code>
+            </Link>
+            <p>SSR build on React.</p>
+          </li>
+        </ol>
+      </Section>
+      <Button onClick={() => window.history.back()}>Back</Button>
+    </BaseLayout>
+  );
 }
 
 export default Projects;
