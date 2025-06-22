67ae3644bf0679a95aa3c2e72c0c0aee12c90a6b
Sergey Bocharov <sergei-b84@bk.ru>
2025-06-22 17:13:41 +0100

    Add prismjs.d.ts

---
+++ @types/prismjs.d.ts
@@ -0,0 +1,5 @@
+declare module 'prismjs/themes/prism-coy.css';
+declare module 'prismjs/plugins/line-numbers/prism-line-numbers.css';
+declare module 'prismjs/components/prism-javascript';
+declare module 'prismjs/components/prism-bash';
+declare module 'prismjs/components/prism-typescript';
--- src/pages/articles/http-api-guideline/index.tsx
+++ src/pages/articles/http-api-guideline/index.tsx
@@ -1,32 +1,59 @@
-import Prism from 'prismjs';
 import Head from 'next/head';
+import { useEffect } from 'react';
 import { BaseLayout } from '../../../components/layouts/BaseLayout';
 import { Button } from '../../../components/Button';
 import { Title } from '../../../components/Title';
 import { Section } from '../../../components/Section';
 import rehypePrism from "rehype-prism";
-import "prismjs/themes/prism-coy.css";
-import "prismjs/plugins/line-numbers/prism-line-numbers.css";
-import Markdown from 'react-markdown'
+import Markdown from 'react-markdown';
 import fs from "fs";
 import path from "path";
-import "prismjs/components/prism-javascript";
-import "prismjs/components/prism-bash";
-import "prismjs/components/prism-typescript";
 
-function ApiGuidline({content}: any) {
+interface ApiGuidelineProps {
+    content: string;
+}
+
+function ApiGuideline({ content }: ApiGuidelineProps) {
+    useEffect(() => {
+        // Загружаем Prism.js только на клиентской стороне
+        const loadPrism = async () => {
+            if (typeof window !== 'undefined') {
+                const Prism = (await import('prismjs')).default;
+                
+                // Загружаем стили
+                // @ts-ignore
+                await import('prismjs/themes/prism-coy.css');
+                // @ts-ignore
+                await import('prismjs/plugins/line-numbers/prism-line-numbers.css');
+                
+                // Загружаем языки
+                // @ts-ignore
+                await import('prismjs/components/prism-javascript');
+                // @ts-ignore
+                await import('prismjs/components/prism-bash');
+                // @ts-ignore
+                await import('prismjs/components/prism-typescript');
+                
+                // Принудительно выделяем код
+                Prism.highlightAll();
+            }
+        };
+        
+        loadPrism();
+    }, []);
+
     return (
         <BaseLayout>
             <Head>
-                <title>Sergey Bocharov - Writing - HTTP API Guidline</title>
+                <title>Sergey Bocharov - Writing - HTTP API Guideline</title>
                 <meta
                     name="viewport"
                     content="initial-scale=1.0, width=device-width"
                 />
             </Head>
-            <Title tag="h1">HTTP API Guidline</Title>
+            <Title tag="h1">HTTP API Guideline</Title>
             <Section>
-            <Markdown rehypePlugins={[rehypePrism]}>{content}</Markdown>
+                <Markdown rehypePlugins={[rehypePrism]}>{content}</Markdown>
             </Section>
             <Button onClick={() => window.history.back()}>Back</Button>
         </BaseLayout>
@@ -38,10 +65,10 @@ export async function getStaticProps() {
     const content = fs.readFileSync(filePath, "utf8");
   
     return {
-      props: {
-        content,
-      },
+        props: {
+            content,
+        },
     };
-  }
+}
 
-export default ApiGuidline;
+export default ApiGuideline;
